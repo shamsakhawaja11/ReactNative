3663a8439922b1f7caa5c281273fff18556e71e7
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-19 18:46:41 +0500

    First native program

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Chine Town</Text>
+      <Text>Resturant & Food</Text>
+      <Text>Order delicious food</Text>
+    </View>
+  );
+}
+
+export default App;
