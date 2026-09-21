dc7c97e6876a7e86085e4c53f97b0c8d4026d39d
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-21 21:41:51 +0500

    View

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import {Text,View} from 'react-native';
+import {StyleSheet, Text,View} from 'react-native';
 
-function App() {
+function RestaurantCard({name,cuisine,rating}){
   return (
-    <View>
-      <Text>Chine Town</Text>
-      <Text>Resturant & Food</Text>
-      <Text>Order delicious food</Text>
+    <View style={styles.card}>
+      <Text>{name}</Text>
+      <Text>{cuisine}</Text>
+      <Text>{rating}</Text>
     </View>
   );
 }
 
+function App() {
+  return (
+    <View>
+      <RestaurantCard 
+        name='China Town'
+        cuisine='Chinese'
+        rating='4.5'
+      
+      />
+      <RestaurantCard
+        name='Monal'
+        cuisine='Pakistani'
+        rating='4.9'
+      />
+      <RestaurantCard
+        name='Asian Wok'
+        cuisine='Chinese'
+        rating='3.9'
+      />
+    </View>
+  );
+}
+const styles=StyleSheet.create({
+  card:{
+    padding:20,
+    margin:10,
+    backgroundColor:'red',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'flex-end'
+  }
+})
 export default App;
