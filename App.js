de236b19b4645e31782ce67a0835802e94d0e85c
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-21 21:50:38 +0500

    Flex

--- App.js
+++ App.js
@@ -38,6 +38,7 @@ const styles=StyleSheet.create({
     padding:20,
     margin:10,
     backgroundColor:'red',
+    flex:1,
     flexDirection:'row',
     justifyContent:'space-between',
     alignItems:'flex-end'
